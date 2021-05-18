778cad6b814278127b651fab736ff121ad7dcbc3
Andres Cusirramos Marquez Mares <andres.cusirramos@icloud.com>
2021-05-18 17:11:33 -0500

    New Voice

--- Friday.py
+++ Friday.py
@@ -1,6 +1,20 @@
 import pyttsx3
+import datetime
 
 
 engine = pyttsx3.init()
-engine.say("Good Morning Sir")
-engine.runAndWait()
+voice = engine.getProperty('voices')
+engine.setProperty('voice', voice[7].id)
+# Possible Voices
+# 10 7 28 26 25
+
+newVoiceRate = 190
+engine.setProperty('rate', newVoiceRate)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+speak("At your servive sir, Welcome home")
